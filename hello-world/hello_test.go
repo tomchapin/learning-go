e008da4878fb1db41131fd912dbcd60454586663
Tom Chapin <tchapin@gmail.com>
2022-05-26 21:22:18 -0700

    adding iteration test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -2,35 +2,36 @@ package main
 
 import "testing"
 
-func assertCorrectMessage(t testing.TB, got, want string) {
+func assertCorrectResult(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("expected %q but got %q", got, want)
 	}
 }
 
 func TestHello(t *testing.T) {
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris", "")
 		want := "Hello, Chris"
-		assertCorrectMessage(t, got, want)
+		assertCorrectResult(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
-		assertCorrectMessage(t, got, want)
+		assertCorrectResult(t, got, want)
 	})
 
 	t.Run("in Spanish", func(t *testing.T) {
 		got := Hello("Elodie", "Spanish")
 		want := "Hola, Elodie"
-		assertCorrectMessage(t, got, want)
+		assertCorrectResult(t, got, want)
 	})
 
 	t.Run("in French", func(t *testing.T) {
 		got := Hello("Elodie", "French")
 		want := "Bonjour, Elodie"
-		assertCorrectMessage(t, got, want)
+		assertCorrectResult(t, got, want)
 	})
 }
